57a7030692b859bf572a836cce87a6c49d050ff2
Dhoifullah Luth Majied <107235147+Ajied21@users.noreply.github.com>
2024-08-19 12:14:16 +0700

    Update Schema_Table.sql

--- SQL/Schema_Table.sql
+++ SQL/Schema_Table.sql
@@ -1,6 +1,6 @@
 -- Tabel Customer Information
 CREATE TABLE customer_information (
-    cust_ID SERIAL PRIMARY KEY,
+    cust_ID SERIAL PRIMARY KEY NOT NULL,
     custfname VARCHAR(255),
     custlname VARCHAR(255),
     address VARCHAR(255),
@@ -9,14 +9,14 @@ CREATE TABLE customer_information (
 
 -- Tabel Payments
 CREATE TABLE payments (
-    payment_ID SERIAL PRIMARY KEY,
+    payment_ID SERIAL PRIMARY KEY NOT NULL,
     customer_ID INT REFERENCES customer_information(cust_ID),
     payment_date TIMESTAMP
 );
 
 -- Tabel Employees
 CREATE TABLE employees (
-    employee_ID SERIAL PRIMARY KEY,
+    employee_ID SERIAL PRIMARY KEY NOT NULL,
     fname VARCHAR(255),
     lname VARCHAR(255),
     job_department VARCHAR(255),
@@ -28,14 +28,14 @@ CREATE TABLE employees (
 
 -- Tabel Room Class
 CREATE TABLE room_class (
-    class_ID SERIAL PRIMARY KEY,
+    class_ID SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(255),
     description VARCHAR(255)
 );
 
 -- Tabel Room Information
 CREATE TABLE room_information (
-    room_ID SERIAL PRIMARY KEY,
+    room_ID SERIAL PRIMARY KEY NOT NULL,
     class_ID INT REFERENCES room_class(class_ID),
     description VARCHAR(255),
     price INT
@@ -43,7 +43,7 @@ CREATE TABLE room_information (
 
 -- Tabel Reservation
 CREATE TABLE reservation (
-    reservation_ID SERIAL PRIMARY KEY,
+    reservation_ID SERIAL PRIMARY KEY NOT NULL,
     customer_ID INT REFERENCES customer_information(cust_ID),
     room_ID INT REFERENCES room_information(room_ID),
     reservation_date TIMESTAMP,
@@ -54,7 +54,7 @@ CREATE TABLE reservation (
 
 -- Tabel Transaction
 CREATE TABLE transactions (
-    transaction_ID SERIAL PRIMARY KEY,
+    transaction_ID SERIAL PRIMARY KEY NOT NULL,
     customer_ID INT REFERENCES customer_information(cust_ID),
     payment_ID INT REFERENCES payments(payment_ID),
     employee_ID INT REFERENCES employees(employee_ID),
@@ -65,7 +65,7 @@ CREATE TABLE transactions (
 
 -- Tabel Reports
 CREATE TABLE reports (
-    report_ID SERIAL PRIMARY KEY,
+    report_ID SERIAL PRIMARY KEY NOT NULL,
     transaction_ID INT REFERENCES transactions(transaction_ID),
     information VARCHAR(255),
     date TIMESTAMP
